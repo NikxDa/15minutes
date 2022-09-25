b2c13c23075ff5cedc3ccd1313d13417d2252c97
iammmai <duongm279@gmail.com>
2022-09-25 10:33:29 +0200

    merge polygons and show marker

--- app/routes/$coordinates.tsx
+++ app/routes/$coordinates.tsx
@@ -1,8 +1,16 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, useSearchParams } from "@remix-run/react";
 import type { LoaderArgs } from "@remix-run/server-runtime";
+import mapboxgl from "mapbox-gl";
 import * as martinez from "martinez-polygon-clipping";
+import { useEffect, useState } from "react";
+import { useMap } from "~/context/MapContext";
 
 const CATEGORIES = ["supermarket", "doctor", "school"];
+const COLOR_CATEGORIES = {
+  supermarket: "#A8DAB5",
+  doctor: "#FDE295",
+  school: "#0080ff",
+};
 
 const getCategoryLocator = async (coordinates: String, category: String) => {
   const result = await fetch(
@@ -13,9 +21,9 @@ const getCategoryLocator = async (coordinates: String, category: String) => {
 
 const getIsochroneForCoord = async (coordinates: Number[]) => {
   const result = await fetch(
-    `https://api.mapbox.com/isochrone/v1/mapbox/cycling/${encodeURIComponent(
+    `https://api.mapbox.com/isochrone/v1/mapbox/walking/${encodeURIComponent(
       coordinates.join(",")
-    )}?contours_minutes=15&polygons=true&denoise=1&access_token=${
+    )}?contours_minutes=15&polygons=true&denoise=1&generalize=100&access_token=${
       process.env.MAPBOX_API_KEY
     }`
   );
@@ -26,37 +34,34 @@ const getPolygonsForCategory = (category: String, places: any) => {
   return Promise.all(
     places.map(async (place: { center: Number[] }) => {
       const isochrone = await getIsochroneForCoord(place.center);
-      // console.log("isocrone", isochrone);
       return isochrone;
     })
   );
 };
 
 const mergePolygons = (polygons: any) => {
-  return polygons.reduce(
-    (acc, curPolygon = []) => {
-      return martinez.union(acc, curPolygon);
-    },
-    [[[]]]
-  );
+  return polygons.reduce((acc, curPolygon) => {
+    return martinez.union(acc, curPolygon.coordinates);
+  }, polygons[0].coordinates);
 };
 
 export const loader = async ({ params }: LoaderArgs) => {
   const { coordinates } = params;
+
   const placesData = await CATEGORIES.reduce(async (res, category) => {
     const places = await getCategoryLocator(coordinates || "", category);
 
     const polygons = (
       await getPolygonsForCategory(category, places.features)
     ).map((polygon) => {
-      return polygon.features[0].geometry.coordinates[0];
+      return polygon.features[0].geometry;
     });
 
     return {
       ...(await res),
       [category]: {
         places: places.features,
-        polygon: mergePolygons(polygons),
+        polygon: { type: "MultiPolygon", coordinates: mergePolygons(polygons) },
       },
     };
   }, {});
@@ -66,7 +71,74 @@ export const loader = async ({ params }: LoaderArgs) => {
 
 export default () => {
   const { data } = useLoaderData<typeof loader>();
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const map = useMap();
 
+  useEffect(() => {
+    if (!map) return;
+
+    map.on("load", () => {
+      Object.entries(data).forEach(([category, categoryData]) => {
+        // if (!selectedCategories.includes(category)) return;
+
+        console.log("addply");
+        map.addSource(category, {
+          type: "geojson",
+          data: {
+            type: "Feature",
+            geometry: categoryData.polygon,
+          },
+        });
+        // Add a new layer to visualize the polygon.
+        map.addLayer({
+          id: category,
+          type: "fill",
+          source: category, // reference the data source
+          layout: {},
+          paint: {
+            "fill-color": COLOR_CATEGORIES[category],
+            "fill-opacity": 0.5,
+          },
+        });
+
+        categoryData.places.forEach((place) => {
+          new mapboxgl.Marker({ color: COLOR_CATEGORIES[category] })
+            .setLngLat(place.geometry.coordinates)
+            .addTo(map);
+        });
+      });
+    });
+  }, [map, data, selectedCategories]);
+
+  const handleSelectCategory = (e) => {
+    console.log("eee", e.target.value);
+    const category = e.target.value;
+    if (selectedCategories.includes(category)) {
+      setSelectedCategories(
+        selectedCategories.filter(
+          (selectedCategory) => selectedCategory !== category
+        )
+      );
+    } else {
+      setSelectedCategories([...selectedCategories, category]);
+    }
+  };
+
+  const renderButtonBar = () => (
+    <div>
+      {CATEGORIES.map((category) => (
+        <button
+          className={`toggle-button`}
+          key={category}
+          value={category}
+          onClick={handleSelectCategory}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
   console.log(data);
-  return JSON.stringify(data, null, 2);
+  // return JSON.stringify(data, null, 2);
+  return renderButtonBar();
 };
